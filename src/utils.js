74d52601d628b25438edca56d8a22a810de12bc2
Dmitry Chizhov <chizhov.dee@gmail.com>
2021-01-26 23:19:30 +0300

    fix eslint errors

--- src/utils.js
+++ src/utils.js
@@ -6,12 +6,13 @@ export function sample(arr) {
   return arr[random(0, arr.length - 1)];
 }
 
+const isMaxGcd = (numbers, value) => !numbers.some((n) => (n % value) !== 0);
+
 export function gcd(...numbers) {
   let maxGcd = numbers.sort((a, b) => a - b)[0];
 
   while (maxGcd > 1) {
-    const isMaxGcd = !numbers.some((n) => (n % maxGcd) !== 0);
-    if (isMaxGcd) break;
+    if (isMaxGcd(numbers, maxGcd)) break;
     maxGcd -= 1;
   }
 
